19e61b5b93ddf8ceba859069df2d7ccae29ddf00
vmrchs <vmrchs@gmail.com>
2023-02-05 12:41:19 -0300

    added comments to the code for better understanding

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Box from "./components/Box";
 import "./App.css";
 
 export default function App() {
+  // Declare new state variable called squares
   const [squares, setSquares] = React.useState(boxes);
 
+  // To change background we create a function that toggles the boolean property of each square
   function toggle(id) {
+    // We use setSquares to update our state, passing down a function to create a memory of our array of objects before any change happens
     setSquares((prevSquares) => {
+      // We map over each square in the array we just created
       return prevSquares.map((square) => {
+        // With a ternary operator we change the state of the square with the same id as the one we clicked
         return square.id === id ? { ...square, on: !square.on } : square;
+        // the other ones return as they were
       });
     });
   }
 
   const squareElements = squares.map((square) => (
+    // We map through squares and assign the data to each one as props
     <Box key={square.id} on={square.on} toggle={() => toggle(square.id)} />
+    // To use the data we need on the Element we click, we pass down a prop arbitrarily named toggle with an arrow function and the data we need as a parameter
   ));
   return <main>{squareElements}</main>;
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,5 +4,6 @@ export default function Box(props) {
   const styles = {
     backgroundColor: props.on ? "#222222" : "transparent",
   };
+  // Once we receive props, we can access React's onClick prop, which is only accessible to native HTML elements
   return <div style={styles} className="box" onClick={props.toggle}></div>;
 }
